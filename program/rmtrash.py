0c705a96c26fc8b3dd2f331e5e46f65d23b2756e
SONIC313 <115035302+SONIC313@users.noreply.github.com>
2022-10-04 23:46:06 +0300

    Update rmtrash.py

--- program/rmtrash.py
+++ program/rmtrash.py
@@ -9,7 +9,7 @@ from driver.decorators import sudo_users_only, errors
 downloads = os.path.realpath("program/downloads")
 raw = os.path.realpath(".")
 
-@Client.on_message(command(["/rmd", "/clear"]) & ~filters.edited)
+@Client.on_message(command(["محو", "/clear"]) & ~filters.edited)
 @errors
 @sudo_users_only
 async def clear_downloads(_, message: Message):
@@ -17,12 +17,12 @@ async def clear_downloads(_, message: Message):
     if ls_dir:
         for file in os.listdir(downloads):
             os.remove(os.path.join(downloads, file))
-        await message.reply_text("✅ **deleted all downloaded files**")
+        await message.reply_text("✅ **تم مسح الملفات المحمله**")
     else:
-        await message.reply_text("❌ **no files downloaded**")
+        await message.reply_text("❌ **لا توجد ملفات محمله لمحوها**")
 
         
-@Client.on_message(command(["/rmw", "/clean"]) & ~filters.edited)
+@Client.on_message(command(["تنظيف", "/clean"]) & ~filters.edited)
 @errors
 @sudo_users_only
 async def clear_raw(_, message: Message):
@@ -31,9 +31,9 @@ async def clear_raw(_, message: Message):
         for file in os.listdir(raw):
             if file.endswith('.raw'):
                 os.remove(os.path.join(raw, file))
-        await message.reply_text("✅ **deleted all raw files**")
+        await message.reply_text("✅ **تم تنظيف و مسح الملفات الزائده**")
     else:
-        await message.reply_text("❌ **no raw files found**")
+        await message.reply_text("❌ **لا توجد ملفات زائده لتنظيفها**")
 
 
 @Client.on_message(command(["/cleanup"]) & ~filters.edited)
@@ -45,6 +45,6 @@ async def cleanup(_, message: Message):
     if ls_dir:
         for dta in os.listdir(pth):
             os.system("rm -rf *.raw *.jpg")
-        await message.reply_text("✅ **cleaned**")
+        await message.reply_text("✅ **تم التنظيف**")
     else:
-        await message.reply_text("✅ **already cleaned**")
+        await message.reply_text("✅ **اكتملت عملية التنظيف بالكامل**")
