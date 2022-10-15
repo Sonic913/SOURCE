cec0d0a898383dc52b7024255013a9a70b41df2b
SONICM313 <115869520+SONICM313@users.noreply.github.com>
2022-10-15 22:00:56 +0300

    Update music.py

--- program/music.py
+++ program/music.py
@@ -132,7 +132,7 @@ async def play(c: Client, m: Message):
                     await suhu.delete()
                     await m.reply_photo(
                         photo=f"{IMG_1}",
-                        caption=f"💡 ***-› تمت الاضافه الى الانتظار** `{pos}`\n\n🏷 **-› العنوان : ** [{songname}]({link})`\n🎧 **-› طݪب :** {requester}",
+                        caption=f"💡 ***-› تمت الاضافه الى الانتظار** `{pos}`\n\n🏷 **-› الاسم :** [{songname}]({link})`\n🎧 **-› طݪب :** {m.from_user.mention()} ",
                         reply_markup=keyboard,
                     )
                 else:
@@ -150,7 +150,7 @@ async def play(c: Client, m: Message):
                         requester = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
                         await m.reply_photo(
                             photo=f"{IMG_2}",
-                            caption=f"🏷 -› العنوان : [{songname}]({link})\n🎧 **-› طݪب :** {requester}",
+                            caption=f"🏷 -› الاسم : [{songname}]({link})\n🎧 **-› طݪب :** {requester}",
                             reply_markup=keyboard,
                         )
                     except Exception as e:
@@ -185,7 +185,7 @@ async def play(c: Client, m: Message):
                                 requester = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
                                 await m.reply_photo(
                                     photo=thumbnail,
-                                    caption=f"💡 ***-› تمت الاضافه الى الانتظار** `{pos}`\n\n🏷 **-› العنوان : ** [{songname}]({url})`\n**⏱ المده :** `{duration}`\n🎧 **-› طݪب :** {requester}",
+                                    caption=f"💡 ***-› تمت الاضافه الى الانتظار** `{pos}`\n\n🏷 **-› الاسم :** [{songname}]({url})`\n**⏱ المده :** `{duration}`\n🎧 **-› طݪب :** {requester}",
                                     reply_markup=keyboard,
                                 )
                             else:
@@ -203,7 +203,7 @@ async def play(c: Client, m: Message):
                                     requester = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
                                     await m.reply_photo(
                                         photo=thumbnail,
-                                        caption=f"🏷 **العنوان : ** [{songname}]({url})\n**⏱ المده :** `{duration}`\n🎧 **طلب : ** {requester}",
+                                        caption=f"🏷 **الاسم :** [{songname}]({url})\n**⏱ المده :** `{duration}`\n🎧 **طلب : ** {requester}",
                                         reply_markup=keyboard,
                                     )
                                 except Exception as ep:
@@ -239,7 +239,7 @@ async def play(c: Client, m: Message):
                             )
                             await m.reply_photo(
                                 photo=thumbnail,
-                                caption=f"💡 ***-› تمت الاضافه الى الانتظار** `{pos}`\n\n🏷 **-› العنوان : ** [{songname}]({url})\n**⏱ المده :** `{duration}`\n🎧 **-› طݪب :** {requester}",
+                                caption=f"💡 ***-› تمت الاضافه الى الانتظار** `{pos}`\n\n🏷 **-› الاسم :** [{songname}]({url})\n**⏱ المده :** `{duration}`\n🎧 **-› طݪب :** {requester}",
                                 reply_markup=keyboard,
                             )
                         else:
@@ -257,7 +257,7 @@ async def play(c: Client, m: Message):
                                 requester = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
                                 await m.reply_photo(
                                     photo=thumbnail,
-                                    caption=f"🏷 **-› العنوان : ** [{songname}]({url})\n**⏱ المده :** `{duration}`\n🎧 **-› طݪب :** {requester}",
+                                    caption=f"🏷 **-› الاسم :** [{songname}]({url})\n**⏱ المده :** `{duration}`\n🎧 **-› طݪب :** {requester}",
                                     reply_markup=keyboard,
                                 )
                             except Exception as ep:
