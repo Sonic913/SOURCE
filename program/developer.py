7bbbd6281ccb698989bc9cb6f1613fb3df6aff49
Sonic913 <116513189+Sonic913@users.noreply.github.com>
2022-10-23 23:11:30 +0300

    Update developer.py

--- program/developer.py
+++ program/developer.py
@@ -31,7 +31,7 @@ async def edit_or_reply(msg: Message, **kwargs):
     await func(**{k: v for k, v in kwargs.items() if k in spec})
 
 
-@Client.on_message(command(["/eval", f"eval{bname}"]) & ~filters.edited)
+@Client.on_message(command(["/eval"]) & ~filters.edited)
 @sudo_users_only
 async def executor(client, message):
     if len(message.command) < 2:
@@ -107,7 +107,7 @@ async def runtime_func_cq(_, cq):
     await cq.answer(runtime, show_alert=True)
 
 
-@Client.on_message(command(["/sh", f"sh{bname}"]) & ~filters.edited)
+@Client.on_message(command(["/sh"]) & ~filters.edited)
 @sudo_users_only
 async def shellrunner(client, message):
     if len(message.command) < 2:
